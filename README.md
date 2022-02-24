d14e3180b0565891f298649a52b695b0707c27d7
dymafr <erwan1309@hotmail.com>
2022-02-24 18:57:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-nynfcn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-nynfcn)
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <template v-for="({ prenom, id }, index) of utilisateurs" :key="id">
+      <li v-if="prenom != 'Paul'">
+        {{ prenom }}
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const utilisateurs = reactive([
+  { prenom: 'Jean', id: 1 },
+  { prenom: 'Paul', id: 2 },
+  { prenom: 'Pierre', id: 3 },
+]);
+
+setTimeout(() => {
+  utilisateurs.push({ prenom: 'Julie', age: 40 });
+}, 3000);
+</script>
+
+<style scoped lang="scss"></style>
